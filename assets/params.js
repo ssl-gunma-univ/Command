581059112f09fb134b33b071f266bfc1280f7035
hiroto-yk <105357836+hiroto-yk@users.noreply.github.com>
2022-11-11 07:56:30 +0900

    add option checkbox

--- assets/params.js
+++ assets/params.js
@@ -5,6 +5,40 @@ export const params = {
     default: '',
     size: 'whole'
   },
+  //option
+  help: {
+    display: '--help',
+    type: 'check',
+    default: false,
+    size: 'whole'
+  },
+  //ls option
+  l: {
+    display: '-l',
+    type: 'check',
+    default: false,
+    size: 'half'
+  },
+  a: {
+    display: '-a',
+    type: 'check',
+    default: false,
+    size: 'half'
+  },
+  //cat option
+  n:{
+    display: '-n',
+    type: 'check',
+    default: false,
+    size: 'half'
+  },
+  A: {
+    display: '-A',
+    type: 'check',
+    default: false,
+    size: 'half'
+  },
+
   contents:{
     display: 'file contents',
     type: 'textarea',
