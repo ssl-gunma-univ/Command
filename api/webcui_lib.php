61eb09935ac2621d2164d18ad4514743a12178a3
hiroto-yk <105357836+hiroto-yk@users.noreply.github.com>
2022-11-10 14:15:19 +0900

    create webcui_lib.php

---
+++ api/webcui_lib.php
@@ -0,0 +1,36 @@
+<?php
+
+function getString($paramName) {
+    $param = $_POST[$paramName];
+    $param = escapeshellarg($param);
+    return $param;
+}
+
+function getFilename($paramName) {
+    $param = $_POST[$paramName];
+    $param = basename($param);
+    $param = escapeshellarg($param);
+    return $param;
+}
+
+function getInt($paramName) {
+    $param = $_POST[$paramName];
+    return (int)$param;
+}
+
+function getFloat($paramName) {
+    $param = $_POST[$paramName];
+    return (float)$param;
+}
+
+function printLine($line) {
+    echo str_replace(" ", "&nbsp;", htmlspecialchars($line, ENT_QUOTES)) . "<br>";
+}
+
+function printOutput($output) {
+    for ($i = 0; $i < count($output); $i++) {
+        printLine($output[$i]);
+    }
+}
+
+?>
